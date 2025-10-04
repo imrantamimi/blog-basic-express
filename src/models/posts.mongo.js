7427d3b91c7759a9827feee0f2b372499750fa2c
Usama Imran Tamimi <imran.tamimi91@gmail.com>
2025-10-04 16:24:53 +0530

    Created posts mongo schema

--- src/models/posts.mongo.js
+++ src/models/posts.mongo.js
@@ -1,2 +1,61 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+
+const postsSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      lowercase: true,
+    },
+    image: {
+      url: {
+        type: String,
+      },
+      altText: {
+        type: String,
+      },
+    },
+    content: {
+      type: String,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Category',
+      required: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  },
+  {
+    timestamps: true, //Add createdAt and updatedAt fields
+  }
+);
+
+//Automatically generate slug name if not set
+
+postsSchema.pre('save', function (next) {
+  if (this.isModified('name')) {
+    this.slug = slugify(this.name, {
+      lower: true,
+      strict: true,
+    });
+  }
+});
+
+// Indexing slug for faster retrieval
+
+postsSchema.index({
+  slug: 1,
+});
+
+module.exports = postsSchema;
